d5f36d76ebe7af000bb427db99c18e97c9687aa1
GYegor <yahor2011@gmail.com>
2019-09-22 19:29:04 +0300

    feat: implement toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  public searchCriterion: string;
+
+  @Output() filterTasks = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  filter() {
+    this.filterTasks.emit(this.searchCriterion)
+  };
 
 }
