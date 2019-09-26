75af8d26b3905b7d08e6bdaed23d7318031a3446
GYegor <yahor2011@gmail.com>
2019-09-26 23:39:30 +0300

    feat: implement board component

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ICardList, ICard } from './../../models/ICardList';
+const cardLists: ICardList[] = [
+  {
+    name:'Backlog',
+    id: '1',
+    cards: [
+      {
+        id:'11',
+        name: '@input',
+        description: 'pass props from @input',
+        dueDate: '23.09.2019',
+        assignee: 'Glueg'
+      },
+      {
+        id:'12',
+        name: '@output',
+        description: 'pass over props from @output'
+      },
+    ]
+  },
+  {
+    name:'In progress',
+    id: '2',
+    cards: []
+  },
+  {
+    name:'Done',
+    id: '3',
+    cards: []
+  },
+];
 
 @Component({
   selector: 'app-board',
@@ -6,10 +37,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  searchCriterion: string;
+  cardLists: ICardList[];
 
   constructor() { }
 
   ngOnInit() {
+    this.cardLists = cardLists;
   }
-
 }
