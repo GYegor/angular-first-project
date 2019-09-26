bf7cefc28586dd989a942925f84bf4cf61da46a9
GYegor <yahor2011@gmail.com>
2019-09-26 23:41:55 +0300

    feat: implement card-list component

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
-import { Card, CardList } from'../../models/CardList'
+import { ICard, ICardList } from'../../models/ICardList'
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.scss']
 })
-export class CardListComponent implements OnInit, CardList {
-  public id: string
-  public name: string
-  public cards: Card[ ]
+export class CardListComponent implements OnInit {
+  @Input() cardList: ICardList;
+  @Input() searchCriterion: string;
 
-  constructor() { }
+  @Output() removeCard = new EventEmitter<ICard>();
+
+  constructor() { };
 
   ngOnInit() {
-  }
+  };
 
 }
