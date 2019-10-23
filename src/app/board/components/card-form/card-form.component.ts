99ecbdc1eba6174172dfea1e4dde6ada88f95808
GYegor <yahor2011@gmail.com>
2019-10-24 02:50:47 +0300

    refactor: add card-form component

---
+++ src/app/board/components/card-form/card-form.component.ts
@@ -0,0 +1,36 @@
+import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import { CardListsService } from '../../services/card-lists.service';
+import { ICardList, ICard } from '../../models/ICardList';
+import { IUser } from '../../models/IUser';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.scss']
+})
+export class CardFormComponent implements OnInit {
+
+  @Input() card: ICard;
+  public cardCopy: ICard;
+  public boardContent: ICardList[];
+  public assignees: IUser[];
+
+  constructor(private fakeStorage: CardListsService, private router: Router) {}
+
+  ngOnInit() {
+    this.boardContent = this.fakeStorage.cardLists;
+    this.assignees = this.fakeStorage.users;
+    this.cardCopy = {...this.card}
+  }
+
+  cancelChanges(event: Event) {
+    event.stopImmediatePropagation();
+    this.router.navigate(['board']);
+  }
+
+  commitCard() {
+    this.fakeStorage.addData(this.cardCopy);
+    this.router.navigate(['board']);
+  }
+}
